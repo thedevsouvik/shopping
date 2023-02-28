607b3c64b3123745569575bce272647ae39ea36c
Souvik Mandal <thedevsouvik@gmail.com>
2023-03-01 01:10:57 +0530

    add app header in app layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
 import "./tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
@@ -10,12 +12,23 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 
 function App() {
   return (
-    <h1 className="flex h-screen w-full items-center justify-center text-center text-5xl md:text-7xl">
-      React
-      <br />
-      Shopping
-      <br />
-      Application
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route
+            index
+            element={
+              <h1 className="flex h-screen w-full items-center justify-center text-center text-5xl md:text-7xl">
+                React
+                <br />
+                Shopping
+                <br />
+                Application
+              </h1>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
