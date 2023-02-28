59269aabb08855b0c2e2e7f30ca6cd2a5b924028
Souvik Mandal <thedevsouvik@gmail.com>
2023-03-01 02:07:07 +0530

    added dynamic products page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
 import HomePage from "./pages/HomePage";
+import ProductCategoryPage from "./pages/ProductsPage";
 import "./tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<HomePage />} />
+          <Route path=":category" element={<ProductCategoryPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
