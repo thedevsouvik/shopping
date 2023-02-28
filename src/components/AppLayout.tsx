15350aa219bf0a8ed831206b590d1779e0b8ea4e
Souvik Mandal <thedevsouvik@gmail.com>
2023-03-01 01:17:07 +0530

    add app footer component

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import AppFooter from "./AppFooter";
 import AppHeader from "./AppHeader";
 
 export default function AppLayout() {
@@ -6,6 +7,7 @@ export default function AppLayout() {
     <>
       <AppHeader />
       <Outlet />
+      <AppFooter />
     </>
   );
 }
