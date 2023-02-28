43cd72ed01b53a27faa97143c4161f99392584fe
Souvik Mandal <thedevsouvik@gmail.com>
2023-03-01 04:41:42 +0530

    fix #9 and some ui improvements

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,6 +1,6 @@
 export default function AppHeader() {
   return (
-    <header className="sticky top-0 flex h-14 w-full items-center bg-white shadow-md md:h-16 md:px-1 md:shadow-lg">
+    <header className="sticky top-0 z-50 flex h-14 w-full items-center bg-white shadow-md md:h-16 md:px-1 md:shadow-lg">
       <p>App header component</p>
     </header>
   );
